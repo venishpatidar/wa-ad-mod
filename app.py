96aa8077040b111b35a4e3b12095878eff381332
Venish Patidar <venishpatidar@gmail.com>
2024-07-19 15:58:24 -0700

    added Model classify path

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from model import Carolina
+
+
+MODEL_PRETRAINED_PATH = "./bloom-fine-tuned"
 
 app = FastAPI()
+model = Carolina(MODEL_PRETRAINED_PATH)
+
 
 @app.get("/")
-def greet_json():
-    return {"Hello": "World!"}
+def greet_json()->None:
+    return "Hello from Carolina"
+
+@app.post("/classify")
+def classify_text(input:str=Body(...))->None:
+    return model.predict(input)
