71f4285deb56c81dc513010fb4b63fb3220c1259
Venish Patidar <venishpatidar@gmail.com>
2024-07-22 02:46:34 -0700

    refactored config file

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    LAUNCH_CONFIG:{
+        useChrome: true,
+        autoRefresh:true,
+        cacheEnabled:false,
+        sessionId: 'hr',
+        skipSessionSave:true
+    },
+    LLM_URL:"Link to you LLM server here"
+};
